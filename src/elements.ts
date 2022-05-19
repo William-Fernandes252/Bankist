aa2c7ff048296ebb1876a4812a564cb3057ae117
William-Fernandes252 <william.winchester1967@gmail.com>
2022-05-19 10:26:07 -0300

    Select UI related elements

--- src/elements.ts
+++ src/elements.ts
@@ -1,4 +1,6 @@
 import $ from 'jquery';
+import logoPath from './assets/img/logo.png';
+import iconPath from './assets/img/icon.png';
 
 export const labelWelcome = $('.welcome');
 export const labelDate = $('.date');
@@ -7,13 +9,16 @@ export const labelSumIn = $('.summary__value--in');
 export const labelSumOut = $('.summary__value--out');
 export const labelSumInterest = $('.summary__value--interest');
 export const labelTimer = $('.timer');
+
 export const containerApp = $('.app');
 export const containerMovements = $('.movements');
+
 export const btnLogin = $('.login__btn');
 export const btnTransfer = $('.form__btn--transfer');
 export const btnLoan = $('.form__btn--loan');
 export const btnClose = $('.form__btn--close');
 export const btnSort = $('.btn--sort');
+
 export const inputLoginUsername = $('.login__input--user');
 export const inputLoginPin = $('.login__input--pin');
 export const inputTransferTo = $('.form__input--to');
@@ -21,3 +26,11 @@ export const inputTransferAmount = $('.form__input--amount');
 export const inputLoanAmount = $('.form__input--loan-amount');
 export const inputCloseUsername = $('.form__input--user');
 export const inputClosePin = $('.form__input--pin');
+
+// Insert logo on page header
+labelWelcome.after(`<img src="${logoPath}" alt="Logo" class="logo" />`);
+
+// Insert icon on page title
+$('head').append(
+    `<link rel="shortcut icon" type="image/png" href="${iconPath}" />`
+);
