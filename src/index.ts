220a9c93fb0981defc96dd54d834c0b02f0bf028
William-Fernandes252 <william.winchester1967@gmail.com>
2022-05-19 10:38:41 -0300

    Attach the event handlers of the ui elements

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,58 @@
 import * as elements from './elements';
+import * as ui from './ui';
 import './assets/style/main.scss';
-import logoPath from './assets/img/logo.png';
-import iconPath from './assets/img/icon.png';
 
-// Insert logo on page header
-elements.labelWelcome.after(
-    `<img src="${logoPath}" alt="Logo" class="logo" />`
-);
+let currentAccount: Account | undefined;
 
-// Insert icon on page title
-$('head').append(
-    `<link rel="shortcut icon" type="image/png" href="${iconPath}" />`
-);
+// Login
+elements.btnLogin.on({
+    click: event => {
+        event.preventDefault();
+        currentAccount = ui.loginAccount();
+    },
+});
+
+// Transferences
+elements.btnTransfer.on({
+    click: event => {
+        event.preventDefault();
+        if (currentAccount && ui.makeTransfer(currentAccount)) {
+            /* Simulate longer processing of the transfer. */
+            setTimeout(() => currentAccount && ui.update(currentAccount), 1000);
+        }
+    },
+});
+
+// Accounts closing
+elements.btnClose.on({
+    click: event => {
+        event.preventDefault();
+        if (ui.closeAccount(currentAccount)) ui.logout();
+    },
+});
+
+// Loan requests
+elements.btnLoan.on({
+    click: event => {
+        event.preventDefault();
+        if (currentAccount && ui.requestLoan(currentAccount)) {
+            /* Simulate longer processing of the loan request. */
+            setTimeout(() => currentAccount && ui.update(currentAccount), 2500);
+        }
+    },
+});
+
+// Sort movements
+let sorted = false;
+elements.btnSort.on({
+    click: () => {
+        if (currentAccount) {
+            ui.displayMovements(
+                currentAccount,
+                elements.containerMovements,
+                !sorted
+            );
+            sorted = !sorted;
+        }
+    },
+});
