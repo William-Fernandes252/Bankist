0f4aeeed4a57ba0e86b873a7e38f46cfec943434
William-Fernandes252 <william.winchester1967@gmail.com>
2022-05-22 13:49:55 -0300

    Declare Bankist namespace

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,17 +1,26 @@
-declare type movement = {
-    value: number;
-    date: Date;
-};
+declare namespace Bankist {
+    const minInterest = 1;
 
-declare interface Account {
-    owner: string;
-    movements: Array<movement>;
-    interestRate: number;
-    username: string;
-    pin: string;
-    balance: number | undefined;
-    locale: string;
-    currency: string;
+    interface IMovement {
+        _value: number;
+        _date: Date;
+    }
+
+    interface IAccount {
+        #owner: string;
+        #username: string;
+        #movements: Array<movement>;
+        #interestRate: number;
+        #username: string;
+        #pin: string;
+        #balance: number | undefined;
+        #locale: string;
+        #currency: string;
+        #balance: number;
+        _validateMovement: (value: number) => boolean;
+        deposit: (value: number) => IAccount;
+        withdraw: (value: number) => IAccount;
+    }
 }
 
 declare module '*.png' {
